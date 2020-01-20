1d643e75e065d88cb8502596d40b9413e5efa90c
Daniel Bisett <daniel@austintatiousdesign.co>
2020-01-20 10:24:48 -0600

    v2.1.6 updated menu to footer menu w icon

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -13,6 +13,7 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 
 ?>
 
+
 <nav id="site-navigation" class="main-navigation nav--toggle-sub nav--toggle-small" aria-label="<?php esc_attr_e( 'Main menu', 'wp-rig' ); ?>"
 	<?php
 	if ( wp_rig()->is_amp() ) {
@@ -35,8 +36,12 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 		<?php
 	}
 	?>
+<?php $menuloop = new \WP_Query( array( 'post_type' => 'menuicon', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
 
-	<button class="menu-toggle" aria-label="<?php esc_attr_e( 'Open menu', 'wp-rig' ); ?>" aria-controls="primary-menu" aria-expanded="false"
+<?php while( $menuloop->have_posts() ) : $menuloop->the_post();
+$menu_icon	= get_field('menu_icon');
+?>
+	<button class="menu-toggle menuTop" aria-label="<?php esc_attr_e( 'Open menu', 'wp-rig' ); ?>" aria-controls="primary-menu" aria-expanded="false"
 		<?php
 		if ( wp_rig()->is_amp() ) {
 			?>
@@ -46,8 +51,25 @@ if ( ! wp_rig()->is_primary_nav_menu_active() ) {
 		}
 		?>
 	>
-		<?php esc_html_e( 'Menu', 'wp-rig' ); ?>
+
+		<img amp-fx="parallax" data-parallax-factor="0.001" src="<?php echo $menu_icon['url']; ?>" alt="<?php echo $menu_icon['alt']; ?>">
+
 	</button>
+	<button class="menu-toggle menuBottom" aria-label="<?php esc_attr_e( 'Open menu', 'wp-rig' ); ?>" aria-controls="primary-menu" aria-expanded="false"
+		<?php
+		if ( wp_rig()->is_amp() ) {
+			?>
+			on="tap:AMP.setState( { siteNavigationMenu: { expanded: ! siteNavigationMenu.expanded } } )"
+			[aria-expanded]="siteNavigationMenu.expanded ? 'true' : 'false'"
+			<?php
+		}
+		?>
+	>
+
+		<img src="<?php echo $menu_icon['url']; ?>" alt="<?php echo $menu_icon['alt']; ?>">
+
+	</button>
+	<?php endwhile;  wp_reset_query(); ?>
 
 	<div class="primary-menu-container">
 		<?php wp_rig()->display_primary_nav_menu( [ 'menu_id' => 'primary-menu' ] ); ?>
