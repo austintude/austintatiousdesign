021e446b7a93a541f9c6b6e24e020469ecbe8f68
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-06-11 18:21:03 -0500

    v2.3.3 updated schema review for google search error fix

--- template-parts/content/block2.php
+++ template-parts/content/block2.php
@@ -11,9 +11,110 @@ $block2_sectioncards	= get_field('block2_sectioncards');
 
 ?>
 
-<section class="block2" id="testimonials">
+<section class="block2" id="testimonials" itemscope itemtype="http://schema.org/LocalBusiness">
+<amp-script layout="container" class="amp-script-sample">
+
+</amp-script>
+<script type="text/plain" target="amp-script" id="hello2">
+{
+"@context" : "http://schema.org",
+"@type" : "LocalBusiness",
+"name" : "Austintatious Design",
+"image" : "https://austintatiousdesign.co/wp-content/uploads/2019/12/austintatious-design-new-banner.png",
+"telephone" : "(469) 850-2946",
+"address" : {
+"@type" : "PostalAddress",
+"streetAddress" : "431 Box Canyon Road",
+"addressLocality" : "Wimberley",
+"addressRegion" : "TX",
+"postalCode" : "78676"
+},
+"openingHoursSpecification" : {
+"@type" : "OpeningHoursSpecification",
+"dayOfWeek" : {
+"@type" : "DayOfWeek",
+"name" : "Monday through Saturday"
+},
+"opens" : "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+"closes" : "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30"
+},
+"review" : [ {
+"@type" : "Review",
+"author" : {
+"@type" : "Person",
+"name" : "Paul Miller"
+},
+"reviewRating" : {
+"@type" : "Rating",
+"ratingValue" : "5",
+"bestRating" : "5",
+"worstRating" : "0"
+},
+"reviewBody" : "Austintatious Design has been working on my business SEO to make it climb up the Google ranks and be more visible online. This is something that I don&#39;t know much about and need help with. I have already seen results that I thought would take longer to see, and am very pleased with the progress and exposure I am receiving. It must be because Daniel knows what he is doing, talking about, and implementing. You can tell this is a passion for him as he is always researching and staying on the cutting edge of his market. #seo #webdesign #DigitalMarketing"
+}, {
+"@type" : "Review",
+"author" : {
+"@type" : "Person",
+"name" : "E. J. Josephson"
+},
+"reviewRating" : {
+"@type" : "Rating",
+"ratingValue" : "5",
+"bestRating" : "5",
+"worstRating" : "0"
+},
+"reviewBody" : "Daniel at Austintatious Design was an absolute lifesaver. Due to an unforeseen issue I needed to change my business name on my website and across all my social media channels. The change also required detailed changes to my content. No easy task. Daniel and I sat down for a strategy meeting, we put an action plan together and he went to work. Within 24 hours (I&#39;m not sure when Daniel sleeps) all the changes were made AND we were under budget! WOW, I was blown away. Based on this experience I plan on having Daniel and Austintatious Design do some graphic, SEO and social media work for me. I highly recommend Austintatious Design!"
+}, {
+"@type" : "Review",
+"author" : {
+"@type" : "Person",
+"name" : "Allison Franklin"
+},
+"reviewRating" : {
+"@type" : "Rating",
+"ratingValue" : "5",
+"bestRating" : "5",
+"worstRating" : "0"
+},
+"reviewBody" : "Daniel at Austintatious Design has proved to be an amazing asset to my company. His web design has garnered many compliments by my consumers as well as other designers. I appreciate his ability to think outside of the box and produce something that really stands out. He has an unbelievable amount of patience which is evident in working with me and my lack of graphic design and SEO knowledge. Not only is he patient and willing to work, the rough drafts of the design for my logo, branding and sample packets were so stellar I had a difficult time chosing which option to accept! Daniel keeps ME on my toes and stays a step ahead of my demands and requirements with idea generation and direction. I&#39;ll second the statement about not knowing when Daniel sleeps as he seems to work hard and around the clock! You can&#39;t go wrong with Austintatious Design."
+}, {
+"@type" : "Review",
+"author" : {
+"@type" : "Person",
+"name" : "Lydia Brasher"
+},
+"reviewRating" : {
+"@type" : "Rating",
+"ratingValue" : "5",
+"bestRating" : "5",
+"worstRating" : "0"
+},
+"reviewBody" : "Representing a small, independent school, I commend Daniel with Austintatious Design for really going the extra mile in quickly learning about our market, taking the time (&amp; patience) to educate us on the ins &amp; outs of digital marketing, and ultimately helping us build a well-rounded plan to boost our SEO--and improve our overall digital marketing strategy--all while keeping our budget and time constraints in check. Thanks Daniel, we are very excited about where we will go from here!"
+} ]
+}
+{
+      "@context": "https://schema.org/",
+      "@type": "AggregateRating",
+      "itemReviewed": {
+        "@type": "LocalBusiness",
+        "image": "https://austintatiousdesign.co/wp-content/uploads/2019/12/austintatious-design-new-banner.png",
+        "name": "Austintatious Design",
+        "telephone" : "(469) 850-2946",
+"address" : {
+"@type" : "PostalAddress",
+"streetAddress" : "431 Box Canyon Road",
+"addressLocality" : "Wimberley",
+"addressRegion" : "TX",
+"postalCode" : "78676"
+}
+      },
+      "ratingValue": "5",
+      "bestRating": "5",
+      "ratingCount": "11"
+    }
+</script>
 <div class="sectionHeader">
-                    <h2 class="">What our clients say
+                    <h2 class="">What <span itemprop="name">Austintatious Design</span>'s clients say
                     </h2>
                     <div class="">
                         <p>Don't take our word for it... take it from them!
@@ -30,23 +131,29 @@ $block2_sectioncards	= get_field('block2_sectioncards');
 					?>
 
                         <div class="sectionCard" id="<?php echo $sectioncard_id; ?>">
-                            <span itemscope="" itemtype="http://schema.org/Review">
+                            <span itemprop="review" itemscope itemtype="http://schema.org/Review">
 
-                                <span itemprop="reviewBody" class="reviewBody">
+                                <span itemscope itemprop="reviewBody" class="reviewBody">
                                     <p class="mb1"><?php echo $sectioncard_content; ?></p>
                                 </span>
 
-                                <small><?php echo $sectioncard_author; ?>, <cite title="Source Title"><?php echo $sectioncard_company; ?></cite></small>
-								<span class="rating" itemprop="reviewRating" itemscope=""
+                                <small><span itemprop="author" itemscope itemtype="http://schema.org/Person"><?php echo $sectioncard_author; ?></span>, <cite title="Source Title"><?php echo $sectioncard_company; ?></cite></small>
+								<span class="rating" itemprop="reviewRating" itemscope
                                     itemtype="http://schema.org/Rating">
                                     <amp-img src="<?php echo get_template_directory_uri() ?>/assets/images/five-star-rating.png" width="208" height="40" class=""
                                         layout="fixed" alt="5 stars for Austintatious Design Co"></amp-img>
-                                </span>
+
                                 <br>
                                 <span itemprop="ratingValue">5</span> / <span itemprop="bestRating">5</span>
-                                <meta itemprop="worstRating" content="0">
+								<meta itemprop="worstRating" content="0">
+								</span>
                             </span>
 						</div>
 						<?php endwhile; ?>
 </div>
+<span itemprop="aggregateRating"
+    itemscope itemtype="http://schema.org/AggregateRating" nodisplay>
+   <meta itemprop="ratingValue" content="5">
+   <meta itemprop="reviewCount" content="11">
+</span>
                 </section>
--- template-parts/content/homeTestimonialQuotes.php
+++ template-parts/content/homeTestimonialQuotes.php
@@ -16,12 +16,93 @@ $more_testimonials_link_txt			= get_field('more_testimonials_link_txt');
 ?>
 
 <section id="testimonialsBlock">
+<amp-script layout="container" script="hello-world" class="amp-script-sample">
+
+      </amp-script>
+<script type="text/plain" target="amp-script" id="hello2">
+{
+  "@context" : "http://schema.org",
+  "@type" : "LocalBusiness",
+  "name" : "Austintatious Design",
+  "image" : "https://austintatiousdesign.co/wp-content/uploads/2019/12/austintatious-design-new-banner.png",
+  "telephone" : "(469) 850-2946",
+  "address" : {
+    "@type" : "PostalAddress",
+    "streetAddress" : "431 Box Canyon Road",
+    "addressLocality" : "Wimberley",
+    "addressRegion" : "TX",
+    "postalCode" : "78676"
+  },
+  "openingHoursSpecification" : {
+    "@type" : "OpeningHoursSpecification",
+    "dayOfWeek" : {
+      "@type" : "DayOfWeek",
+      "name" : "Monday through Saturday"
+    },
+    "opens" : "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "closes" : "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30"
+  },
+  "review" : [ {
+    "@type" : "Review",
+    "author" : {
+      "@type" : "Person",
+      "name" : "Paul Miller"
+    },
+    "reviewRating" : {
+      "@type" : "Rating",
+      "ratingValue" : "5",
+      "bestRating" : "5",
+      "worstRating" : "0"
+    },
+    "reviewBody" : "Austintatious Design has been working on my business SEO to make it climb up the Google ranks and be more visible online. This is something that I don&#39;t know much about and need help with. I have already seen results that I thought would take longer to see, and am very pleased with the progress and exposure I am receiving. It must be because Daniel knows what he is doing, talking about, and implementing. You can tell this is a passion for him as he is always researching and staying on the cutting edge of his market. #seo #webdesign #DigitalMarketing"
+  }, {
+    "@type" : "Review",
+    "author" : {
+      "@type" : "Person",
+      "name" : "E. J. Josephson"
+    },
+    "reviewRating" : {
+      "@type" : "Rating",
+      "ratingValue" : "5",
+      "bestRating" : "5",
+      "worstRating" : "0"
+    },
+    "reviewBody" : "Daniel at Austintatious Design was an absolute lifesaver. Due to an unforeseen issue I needed to change my business name on my website and across all my social media channels. The change also required detailed changes to my content. No easy task. Daniel and I sat down for a strategy meeting, we put an action plan together and he went to work. Within 24 hours (I&#39;m not sure when Daniel sleeps) all the changes were made AND we were under budget! WOW, I was blown away. Based on this experience I plan on having Daniel and Austintatious Design do some graphic, SEO and social media work for me. I highly recommend Austintatious Design!"
+  }, {
+    "@type" : "Review",
+    "author" : {
+      "@type" : "Person",
+      "name" : "Allison Franklin"
+    },
+    "reviewRating" : {
+      "@type" : "Rating",
+      "ratingValue" : "5",
+      "bestRating" : "5",
+      "worstRating" : "0"
+    },
+    "reviewBody" : "Daniel at Austintatious Design has proved to be an amazing asset to my company. His web design has garnered many compliments by my consumers as well as other designers. I appreciate his ability to think outside of the box and produce something that really stands out. He has an unbelievable amount of patience which is evident in working with me and my lack of graphic design and SEO knowledge. Not only is he patient and willing to work, the rough drafts of the design for my logo, branding and sample packets were so stellar I had a difficult time chosing which option to accept! Daniel keeps ME on my toes and stays a step ahead of my demands and requirements with idea generation and direction. I&#39;ll second the statement about not knowing when Daniel sleeps as he seems to work hard and around the clock! You can&#39;t go wrong with Austintatious Design."
+  }, {
+    "@type" : "Review",
+    "author" : {
+      "@type" : "Person",
+      "name" : "Lydia Brasher"
+    },
+    "reviewRating" : {
+      "@type" : "Rating",
+      "ratingValue" : "5",
+      "bestRating" : "5",
+      "worstRating" : "0"
+    },
+    "reviewBody" : "Representing a small, independent school, I commend Daniel with Austintatious Design for really going the extra mile in quickly learning about our market, taking the time (&amp; patience) to educate us on the ins &amp; outs of digital marketing, and ultimately helping us build a well-rounded plan to boost our SEO--and improve our overall digital marketing strategy--all while keeping our budget and time constraints in check. Thanks Daniel, we are very excited about where we will go from here!"
+  } ]
+}
+</script>
     <div class="testimonialsBlockTitle">
     <h2><?php echo $testimonials_block_title; ?></h2>
 </div>
     <ol class="testimonialsList testimonialsList">
         <!-- insert acf sub-repeater here -->
-        <amp-carousel height="600" 
+        <amp-carousel height="600"
 		width="1200"
 		layout="responsive"
 		type="slides"
@@ -53,21 +134,21 @@ $overall = get_sub_field('overall');
                             <ul>
                                 <li>
                                     <div class="leftItem">
-                                    Star Rating: 
+                                    Star Rating:
                                     </div>
                                     <div class="rightItem">
                                     <span class="rating" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
                                         <meta itemprop="worstRating" content="0">
                                             <span itemprop="ratingValue">
                                             <?php echo $overall; ?>
-                                            </span> / 
+                                            </span> /
                                             <span itemprop="bestRating">
-                                            <?php echo $overall; ?> 
+                                            <?php echo $overall; ?>
                                                 </span>
                                     </span>
                                         </div>
                                 </li>
-                                
+
                             </ul>
                         </div>
                     </div>
@@ -77,17 +158,17 @@ $overall = get_sub_field('overall');
                 <cite class="testimonialsQuote">
                     <span itemprop="author" itemscope itemtype="http://schema.org/Person">
                     <?php echo $testimonial_author; ?>
-                    </span> - 
+                    </span> -
                     <meta itemprop="datePublished" content="<?php echo $testimonial_date; ?>">
                     <span class="testimonialDate">
                         <?php echo $testimonial_date; ?>
                     </span>
 
                 </cite>
-                
+
             </blockquote>
-            
-                
+
+
             </li>
             <?php endwhile; ?>
             </amp-carousel>
