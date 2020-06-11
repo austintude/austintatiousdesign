45c78612349dd35ed50589da140a0a4921d2af4e
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-06-11 14:49:37 -0500

    v2.3.2 footer-info updates

--- template-parts/footer/info-blog.php
+++ template-parts/footer/info-blog.php
@@ -7,7 +7,19 @@
 
 namespace WP_Rig\WP_Rig;
 
-			$contact_us_phone			= get_field('contact_us_phone');
+
+
+?>
+
+
+
+<div class="site-info">
+
+
+<?php $footerloop = new \WP_Query( array( 'post_type' => 'footer_info', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+<?php while( $footerloop->have_posts() ) : $footerloop->the_post();
+$contact_us_phone			= get_field('contact_us_phone');
 			$contact_us_phone_link			= get_field('contact_us_phone_link');
 			$contact_us_street			= get_field('contact_us_street');
 			$contact_us_street_suite			= get_field('contact_us_street_suite');
@@ -15,23 +27,28 @@ namespace WP_Rig\WP_Rig;
 			$contact_us_state			= get_field('contact_us_state');
 			$contact_us_zip			= get_field('contact_us_zip');
 			$contact_us_open_datetime			= get_field('contact_us_open_datetime');
+			$contact_us_open_datetime_extra			= get_field('contact_us_open_datetime_extra');
 			$contact_us_open_days_times			= get_field('contact_us_open_days_times');
+			$contact_us_open_days_times_extra			= get_field('contact_us_open_days_times_extra');
 			$contact_us_closed			= get_field('contact_us_closed');
 			$contact_us_form			= get_field('contact_us_form');
 			$cta_loading_image			= get_field('cta_loading_image');
 			$middle_images			= get_field('middle_images');
 			$right_images			= get_field('right_images');
 			$facebook_link			= get_field('facebook_link');
+			$facebook_icon			= get_field('facebook_icon');
 			$instagram_link			= get_field('instagram_link');
+			$instagram_icon		= get_field('instagram_icon');
+			$linkedin_icon			= get_field('linkedin_icon');
 			$linkedin_link			= get_field('linkedin_link');
-
-?>
-
-
-<div class="site-info">
-
-
-
+			$google_map			= get_field('google_map');
+			$station_identification			= get_field('station_identification');
+			$station_status			= get_field('station_status');
+			$middle_image_1			= get_field('middle_image_1');
+			$middle_image_2			= get_field('middle_image_2');
+			$right_image_1			= get_field('right_image_1');
+			$right_image_2			= get_field('right_image_2');
+			?>
 
 
 	<div class="topBlock">
@@ -44,64 +61,50 @@ namespace WP_Rig\WP_Rig;
 
         <div id="address">
             <address property="address" typeof="PostalAddress">
-			<h5>Austintatious Design</h5>
-			<h5><span property="streetAddress">
-				<!-- <?php echo $contact_us_street; ?> -->
-				431 Box Canyon Rd.
+			<h5>
+			<?php echo $station_identification; ?>
+			</h5>
+			<h5><span property="streetAddress"><?php echo $contact_us_street; ?>
 		<br>
-		<!-- <?php echo $contact_us_street_suite; ?> -->
-		Suite B
-	</span>
+		<?php echo $contact_us_street_suite; ?></span>
             <br>
-            <span property="addressLocality">
-				<!-- <?php echo $contact_us_city; ?> -->
-				Wimberley,
-			</span>
+            <span property="addressLocality"><?php echo $contact_us_city; ?></span>,
 
-			<span property="addressRegion">
-				<!-- <?php echo $contact_us_state; ?> -->
-				Texas
-			</span>
-			<!-- <?php echo $contact_us_zip; ?> -->
-			78676
-		</h5>
+			<span property="addressRegion"><?php echo $contact_us_state; ?></span> <?php echo $contact_us_zip; ?></h5>
 </address>
-        </div>
+		</div>
+		<h5>
+		<?php echo $station_status; ?>
+		</h5>
 </section>
 <div id="phone">
-			<span property="telephone"><a href="tel:4698502946">
-				<!-- <?php echo $contact_us_phone; ?> -->
-				(469) 850-2946
-			</a></span>
+			<span property="telephone"><a href="tel:<?php echo $contact_us_phone_link; ?>"><?php echo $contact_us_phone; ?></a></span>
 		</div>
 <div id="hours">
 	<h4>Hours of Operation</h4>
 			<ul>
 				<li>
-				<time itemprop="openingHours" datetime="Mo - Sa 8:00-19:00">
-					<!-- <?php echo $contact_us_open_days_times; ?> -->
-					Monday - Saturday 8:00am - 7:00pm
-				</time>
+				<time itemprop="openingHours" datetime="<?php echo $contact_us_open_datetime; ?>"><?php echo $contact_us_open_days_times; ?></time>
 				</li>
+				<?php if ($contact_us_open_datetime_extra != null) { ?>
+				<li>
+				<time itemprop="openingHours" datetime="<?php echo $contact_us_open_datetime_extra; ?>"><?php echo $contact_us_open_days_times_extra; ?></time>
+				</li>
+				<?php } ?>
 
 				<li>
-				 <!-- <?php echo $contact_us_closed; ?> -->
-				 Sunday
-				  - Closed
+				 <?php echo $contact_us_closed; ?> - Closed
 				</li>
 			</ul>
 
         </div>
         <div id="googleMap">
-        <amp-iframe width="600" height="450" layout="responsive"
+        <iframe width="600" height="450" layout="responsive"
 			  sandbox="allow-scripts allow-same-origin allow-popups"
 			  frameborder="0"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13820.148829481264!2d-98.077079!3d30.007088!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x71e354a4170d40c3!2sAustintatious%20Design!5e0!3m2!1sen!2sus!4v1575479234268!5m2!1sen!2sus">
-    <amp-img layout="fill"
-             src="<?php echo get_template_directory_uri() ?>/assets/images/Google-Map-theme.jpg"
-			 placeholder>
-			</amp-img>
-  </amp-iframe>
+              src="<?php echo $google_map; ?>">
+
+  </iframe>
 </div>
 </div>
 <div class="softBlock">
@@ -111,35 +114,35 @@ namespace WP_Rig\WP_Rig;
 
 	<div id="themeSocial">
 		<div id="socialIcon1">
-			<a href="https://fb.me/austintude" rel="noopener" target="_blank">
+			<a href="<?php echo $facebook_link; ?>" rel="noopener"  target="_blank" aria-label="<?php echo $facebook_link; ?>">
 			<amp-img
-				src="<?php echo get_template_directory_uri() ?>/assets/images/facebook-icon.png"
-				width="1"
-				height="1"
+				src="<?php echo $facebook_icon['url']; ?>"
+				width="5"
+				height="5"
 				layout="responsive"
-				alt="facebook icon">
+				alt="<?php echo $facebook_icon['alt']; ?>">
 			</amp-img>
 </a>
 		</div>
 		<div id="socialIcon2">
-		<a href="https://instagram.com/austintude" rel="noopener" target="_blank">
-			<amp-img
-				src="<?php echo get_template_directory_uri() ?>/assets/images/instagram-icon.png"
-				width="1"
-				height="1"
+		<a href="<?php echo $instagram_link; ?>" rel="noopener" target="_blank" aria-label="<?php echo $instagram_link; ?>">
+		<amp-img
+				src="<?php echo $instagram_icon['url']; ?>"
+				width="5"
+				height="5"
 				layout="responsive"
-				alt="instagram icon">
+				alt="<?php echo $instagram_icon['alt']; ?>">
 			</amp-img>
 </a>
 		</div>
 		<div id="socialIcon3">
-		<a href="https://linkedin.com/in/austintude" rel="noopener" target="_blank">
+		<a href="<?php echo $linkedin_link; ?>" rel="noopener" target="_blank" aria-label="<?php echo $linkedin_link; ?>">
 			<amp-img
-				src="<?php echo get_template_directory_uri() ?>/assets/images/linkedin-icon.png"
-				width="1"
-				height="1"
+				src="<?php echo $linkedin_icon['url']; ?>"
+				width="5"
+				height="5"
 				layout="responsive"
-				alt="linkedin icon">
+				alt="<?php echo $linkedin_icon['alt']; ?>">
 			</amp-img>
 </a>
 		</div>
@@ -150,24 +153,18 @@ namespace WP_Rig\WP_Rig;
 	<div class="blockTitle">
 		<h4><?php bloginfo( 'name' ); ?></h4>
 </div>
-<div class="blockImages">
 
-		<amp-img
-		src="../../wp-content/uploads/2019/12/bing-ads-cert.jpg"
-  width="640"
-			height="640"
-			layout="intrinsic"
+<div class="blockImages">
+		<img
+		src="<?php echo $middle_image_1['url']; ?>"
+
 			lightbox
-			alt="Bing Cert">
-		</amp-img>
-		<amp-img
-		src="../../wp-content/uploads/2019/12/adwords-search-marketing-cert.jpg"
-  width="640"
-			height="640"
-			layout="intrinsic"
+			alt="<?php echo $middle_image_1['alt']; ?>">
+		<img
+		src="<?php echo $middle_image_2['url']; ?>"
+
 			lightbox
-			alt="Google Ads Cert">
-		</amp-img>
+			alt="<?php echo $middle_image_2['alt']; ?>">
 
 </div>
 </div>
@@ -177,34 +174,29 @@ namespace WP_Rig\WP_Rig;
 </div>
 <div class="blockImages">
 
-		<amp-img
-		src="../../wp-content/uploads/2019/12/LAustin-Translation-Digital-Marketing-scaled.jpg"
-  width="2500"
-			height="8100"
-			layout="intrinsic"
+		<img
+		src="<?php echo $right_image_1['url']; ?>"
 			lightbox
-			alt="Onsite and Offsite SEO">
-		</amp-img>
-		<amp-img
-		src="../../wp-content/uploads/2019/12/danielbisett-resume-scaled.jpg"
-  width="2500"
-			height="8100"
-			layout="intrinsic"
+			alt="<?php echo $right_image_1['alt']; ?>">
+		<img
+		src="<?php echo $right_image_2['url']; ?>"
 			lightbox
-			alt="Daniel Bisett CV">
-		</amp-img>
+			alt="<?php echo $right_image_2['alt']; ?>">
 
 </div>
 </div>
 
 </div>
+<?php endwhile;  wp_reset_query(); ?>
 <div class="baseBlock">
 
-<div id="themeLogo">
+<div id="themeLogo" aria-label="custom logo link">
 <?php the_custom_logo(); ?>
 </div>
+
 <div class="builtBy">
-	<a href="<?php echo esc_url( __( 'https://austintatiousdesign.co/', 'wp-rig' ) ); ?>">
+
+	<a href="<?php echo esc_url( __( 'https://austintatiousdesign.co/', 'wp-rig' ) ); ?>" rel="noopener" aria-label="Link to Austintatious design">
 		<?php
 		/* translators: %s: CMS name, i.e. WordPress. */
 		printf( esc_html__( 'Proudly built and maintained by %s', 'wp-rig' ), 'Austintatious Design' );
@@ -214,3 +206,4 @@ namespace WP_Rig\WP_Rig;
 </div>
 
 </div><!-- .site-info -->
+
