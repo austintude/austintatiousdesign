5873780cc771b896d2cc0b06ea418e2d0b231417
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-06-11 15:05:12 -0500

    v2.3.2

--- header-custom.php
+++ header-custom.php
@@ -43,7 +43,7 @@ $tagline			= get_field('tagline');
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wp-rig' ); ?></a>
 
 	<header id="masthead" class="site-header">
-
+	<?php get_template_part( 'template-parts/header/custom_header' ); ?>
 		<?php get_template_part( 'template-parts/header/custom-page-branding' ); ?>
 
 
