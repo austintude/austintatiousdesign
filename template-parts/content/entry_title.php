86d2de04476eece753e2673c333edf5e108c8e01
Daniel Bisett <development@wimberleyvalleyradio.org>
2020-06-17 11:05:44 -0500

    v2.3.5 fixed double featured image display on single blog post

--- template-parts/content/entry_title.php
+++ template-parts/content/entry_title.php
@@ -20,13 +20,7 @@ if ( is_singular( get_post_type() ) ) {
 	data-videoid="<?php echo $youtube_link; ?>"
 	>
   </amp-youtube>
-  <?php else : ?>
-		<?php if ( has_post_thumbnail('') ) { //check for feature image}
-                $post_thumbnail = get_the_post_thumbnail_url();
-                ?>
-                <amp-img itemprop="image" width="300" height="230" src="<?php echo $post_thumbnail; ?>" alt="Austintatious Design and: <?php the_title(''); ?>" class="" layout="responsive"></amp-img>
 
-                <?php } ?>
 
 <?php endif; ?>
   <?php
